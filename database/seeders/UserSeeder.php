4a2a43b75d3b9beca0b12a18515ec25e6ab0dec0
emilsonsn <emilsonsn2@gmail.com>
2025-01-28 21:31:48 -0300

    feat: adiciona usuários ao seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Carbon\Carbon;
+use Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,12 +15,48 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::updateOrCreate([
-            'email' => 'admin@admin',
-        ],[
-            'name' => 'Admin',            
-            'password' => 'admin',
-            'email_verified_at' => Carbon::now(),
-        ]);
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin',
+                'password' => 'admin',
+            ],
+            [
+                'name' => 'Henrique',
+                'email' => 'Henrique_cr2@hotmail.com',
+                'password' => 'Palmeiras10%',
+            ],
+            [
+                'name' => 'Silvio Cesar',
+                'email' => 'silvio.cesar99@mail.com',
+                'password' => '071407',
+            ],
+            [
+                'name' => 'Fernando Tavares',
+                'email' => 'fernando.tavares_7@hotmail.com',
+                'password' => '12230422',
+            ],
+            [
+                'name' => 'Priscilla Ferreira',
+                'email' => 'pri.scillaferreiraeju@gmail.com',
+                'password' => 'jullya321!',
+            ],
+            [
+                'name' => 'Allan Revendedor',
+                'email' => 'allan.revendedor@gmail.com',
+                'password' => 'jullya123@',
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']],
+                [
+                    'name' => $user['name'],
+                    'password' => Hash::make($user['password']),
+                    'email_verified_at' => Carbon::now(),
+                ]
+            );
+        }
     }
 }
